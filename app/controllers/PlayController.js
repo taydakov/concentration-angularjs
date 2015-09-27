6e2b9ee85f54c31b05785b1811dcf2933d0bf0e1
Lev Taydakov <lev.taydakov@gmail.com>
2015-09-26 17:17:51 -0700

    Add card flip logic, refactor

--- app/controllers/PlayController.js
+++ app/controllers/PlayController.js
@@ -10,19 +10,74 @@
     .module('concentrationGame')
     .controller('PlayController', PlayController);
 
-  PlayController.$inject = ['$routeParams', '$location'];
+  PlayController.$inject = ['$routeParams', '$location', '$scope'];
 
-  function PlayController($routeParams, $location) {
+  function PlayController($routeParams, $location, $scope) {
 
+    /* Private Constants & Variables */
+    var maxCardsNum = 52;
+    var minCardsNum = 2;
     var self = this;
-    var cardsNum = parseInt($routeParams.pairsNum) * 2;
+    var cardsNum    = parseInt($routeParams.pairsNum) * 2;
+    var isFlipped   = false;
+    var flippedCard = null;
+
+    /* Public data */
+    self.cards = [];
+
+    /* Params verification */
+    if (cardsNum > maxCardsNum) {
+      cardsNum = maxCardsNum
+    } else if (cardsNum < minCardsNum) {
+      cardsNum = minCardsNum;
+    }
 
     console.log('controller started cardsNum = ', cardsNum);
 
-    self.cards = [];
+    /* Fill cards array */
     for (var i = 0; i < cardsNum; i++) {
-      self.cards.push({});
+      self.cards.push({
+        number:  i,
+        ranking: i,
+        color:   true, // false - black, true - red
+        flipped: false,
+        matched: false
+      });
     }
+
+    /* Event Handlers */
+    self.handleCardClick = function (card) {
+      console.log('handleCardClick card = ', card);
+      // Exit if user clicked the same card twice
+      if (flippedCard && (flippedCard === card)) {
+        return;
+      }
+
+      if (!isFlipped && !flippedCard) {
+        // Flip the first card in a pair
+        card.flipped = true;
+        
+        isFlipped   = true;
+        flippedCard = card;
+      } else if (isFlipped && flippedCard) {
+        // Flip the second card in a pair and check
+        card.flipped = true;
+
+        isFlipped   = true;
+        flippedCard = null;
+      } else if (isFlipped && !flippedCard) {
+        // Flip all the unmatched cards back and flip the current one
+        self.cards.forEach(function (card) {
+          if (!card.matched) {
+            card.flipped = false;
+          }
+        });
+        card.flipped = true;
+
+        isFlipped   = true;
+        flippedCard = card;
+      }
+    };
 
   }
 
